eae479b1239a7f29147d62e5679c055773915cdf
michaelmarchese <charlesm25@outlook.com>
2023-03-07 16:13:08 -0500

    change to both

--- Both.py
+++ Both.py
@@ -1,4 +1,4 @@
-''' Code from ROS2 Doc
+''' Code from ROS2 Docs
 Modifications by Briana Bouchard
 
 This code creates a publisher node and publishes a string to a topic with a counter every 0.5 seconds.
